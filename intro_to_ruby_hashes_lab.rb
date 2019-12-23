852885ae719f0f976d943e21d3a9a6bd390692f4
Aaron Burmeister <f34r91@gmail.com>
2019-12-23 21:31:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,10 +20,16 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_gen_hash = {
+    id: 5
+  }
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  create_hash = {
+    key => value
+  }
 end
 
 def read_from_hash(hash, key)
