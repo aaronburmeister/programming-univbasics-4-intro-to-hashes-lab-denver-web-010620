5b81b1824ee83efc365d7d977aaae7b5a19314f9
Aaron Burmeister <f34r91@gmail.com>
2019-12-23 21:27:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,14 @@
 def new_hash
   # return an empty hash
+  Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  myhash = {
+    name: "Aaron",
+    age: 28
+  }
 end
 
 def pioneer
